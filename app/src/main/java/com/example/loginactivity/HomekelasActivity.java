89e8ae10b087ecf4dcf9beb367b9bfc413cb1a7f
ummalatulkamilah <55658527+ummalatulkamilah@users.noreply.github.com>
2020-05-12 08:30:36 +0700

    tambah fragmen home

--- app/src/main/java/com/example/loginactivity/HomekelasActivity.java
+++ app/src/main/java/com/example/loginactivity/HomekelasActivity.java
@@ -31,7 +31,7 @@ public class HomekelasActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_akunsaya, R.id.nav_banntuan, R.id.nav_logout, R.id.nav_logout)
+                R.id.nav_akunsaya, R.id.nav_banntuan, R.id.nav_logout, R.id.nav_home)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
