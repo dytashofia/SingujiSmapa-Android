2cf851a6eaef2d240ff01a2b20da194eb158c57b
bagoesihsant <61548001+bagoesihsant@users.noreply.github.com>
2020-06-22 15:03:06 +0700

    Undone

--- app/src/main/java/com/example/loginactivity/LoginActivity.java
+++ app/src/main/java/com/example/loginactivity/LoginActivity.java
@@ -159,7 +159,6 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(new Intent(LoginActivity.this, LupaPasswordActivity.class));
     }
 
-
 }
 
 
--- app/src/main/java/com/example/loginactivity/MainActivity.java
+++ app/src/main/java/com/example/loginactivity/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.loginactivity;
 
-import android.os.Bundle;
-import android.view.MenuItem;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
